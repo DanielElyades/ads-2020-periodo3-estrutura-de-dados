9e26b3752fdb6b97bad5d120b3205ea93017f74b
Daniel Elyades Oliveira <danielelyades@gmail.com>
2020-08-06 01:02:50 -0300

    Inicio do projeto

---
+++ aula01-vetores-joseluis/src/vetor1main/Vetor1.java
@@ -0,0 +1,23 @@
+package vetor1main;
+
+import java.util.Scanner;
+
+public class Vetor1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner tc = new Scanner(System.in);
+		
+		int[] vet = new int[16];
+		for(int i = 0; i <= 16; i++) {
+	    
+			System.out.println("Informe um nÚmero: ");
+			int n = tc.nextInt();
+			
+			
+		}
+
+	}
+
+}
